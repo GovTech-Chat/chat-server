70e0c5cca6f7c26e1af97040fd982f173f3506c9
peikfeng <peikfeng@gmail.com>
2023-12-25 22:54:16 +0800

    WebSocket config and controller

---
+++ src/main/java/com/bee/chat/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.bee.chat.controller;
+
+import com.bee.chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/register")
+    @SendTo("/topic/lunch")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/send")
+    @SendTo("/topic/lunch")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
---
+++ src/main/java/com/bee/chat/model/MessageType.java
@@ -0,0 +1,5 @@
+package com.bee.chat.model;
+
+public enum MessageType {
+    CHAT, LEAVE, JOIN
+}
